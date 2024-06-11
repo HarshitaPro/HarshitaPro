00925484d91b6e68eef4b3b5b05ab54f4b6c2000
HarshitaPro <kc619496@gmail.com>
2024-06-11 06:48:40 -0700

    Your commit message

---
+++ Inheritance/src/Basic_Inheritance/Employee.java
@@ -0,0 +1,13 @@
+package Basic_Inheritance;
+
+public class Employee {
+	int salary;
+	String Name;
+	public Employee(int salary, String name) {
+		super();
+		this.salary = salary;
+		Name = name;
+	}
+	
+
+}
