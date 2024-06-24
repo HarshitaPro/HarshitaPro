86bffa239bc1d6cddc6648f4497dea4b3c8005e1
HarshitaPro <kc619496@gmail.com>
2024-06-24 07:40:50 -0700

    Abstract class Interface code in uploaded

--- june13/src/june20ArrayQuestion/ArrayQue.java
+++ june13/src/june20ArrayQuestion/ArrayQue.java
@@ -22,7 +22,13 @@ public class ArrayQue {
 	System.out.println("number of 0's="+count0);
 	System.out.println("number of 1's="+count1);
 	System.out.println("number of 2's="+count2);
+	
+	int arr1[][]= {{3} ,{2}};
+	System.out.println(arr1.length);
+	System.out.println(arr1[0].length);
 
 	}
+	
+	
 
 }
---
+++ june13/src/this_keyword/Test.java
@@ -0,0 +1,20 @@
+package this_keyword;
+
+public class Test extends FinalVariable{
+	
+	Test(){
+		super("NRI");
+	}
+
+	@Override
+	public double CalculateArea()
+	{
+		
+	}
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
